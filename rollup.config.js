6ebe85ce07897cddd83d4d44a821871105982a5e
Yoshihiro Seki <nikochan2k@gmail.com>
2022-09-25 07:39:47 +0900

    modify build configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,49 +1,49 @@
-import * as path from 'path';
-import webWorkerLoader from 'rollup-plugin-web-worker-loader';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import alias from '@rollup/plugin-alias';
-import { terser } from 'rollup-plugin-terser';
+import * as path from "path";
+import webWorkerLoader from "rollup-plugin-web-worker-loader";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import alias from "@rollup/plugin-alias";
+import { terser } from "rollup-plugin-terser";
 
 function getConfig(entry, filename, perf) {
   // Remove the extension
-  let basename = filename.replace(/\.[^.]*/, '');
+  let basename = filename.replace(/\.[^.]*/, "");
 
   return {
     input: entry,
     output: {
-      dir: perf ? 'dist/perf' : 'dist',
+      dir: perf ? "dist/perf" : "dist",
       entryFileNames: filename,
       chunkFileNames: `${basename}-[name]-[hash].js`,
-      format: 'esm',
-      exports: 'named'
+      format: "esm",
+      exports: "named",
     },
     plugins: [
       !perf &&
         alias({
           entries: {
-            'perf-deets': path.resolve(
+            "perf-deets": path.resolve(
               __dirname,
-              './node_modules/perf-deets/noop.js'
-            )
-          }
+              "./node_modules/perf-deets/noop.js"
+            ),
+          },
         }),
       webWorkerLoader({
         pattern: /.*\/worker\.js/,
-        targetPlatform: 'browser',
+        targetPlatform: "browser",
         external: [],
-        plugins: [terser()]
+        plugins: [terser()],
       }),
-      nodeResolve()
+      nodeResolve(),
     ],
-    ...(perf ? { external: ['perf-deets'] } : {})
+    ...(perf ? { external: ["perf-deets"] } : {}),
   };
 }
 
 export default [
-  getConfig('src/index.js', 'index.js'),
-  getConfig('src/memory/backend.js', 'memory-backend.js'),
-  getConfig('src/indexeddb/backend.js', 'indexeddb-backend.js'),
-  getConfig('src/indexeddb/main-thread.js', 'indexeddb-main-thread.js'),
-  getConfig('src/indexeddb/backend.js', 'indexeddb-backend.js', true),
-  getConfig('src/indexeddb/main-thread.js', 'indexeddb-main-thread.js', true)
+  getConfig("lib/index.js", "index.js"),
+  getConfig("lib/memory/backend.js", "memory-backend.js"),
+  getConfig("lib/indexeddb/backend.js", "indexeddb-backend.js"),
+  getConfig("lib/indexeddb/main-thread.js", "indexeddb-main-thread.js"),
+  getConfig("lib/indexeddb/backend.js", "indexeddb-backend.js", true),
+  getConfig("lib/indexeddb/main-thread.js", "indexeddb-main-thread.js", true),
 ];
